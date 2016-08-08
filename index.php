5119b18d09b50e2feffb84a0e6dc8d17d6d5687f
Khoirul Umam <khoirul.umam35@gmail.com>
2016-08-08 20:33:45 +0700

    Update introducing
    Menambahkan keterangan mengajar matakuliah apa

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 	echo "<h1>Halo!</h1>";
-	echo "<p>Selamat datang di website saya. Nama saya Khoirul Umam. Saya dari STIKOM PGRI Banyuwangi</p>";
+	echo "<p>Selamat datang di website saya. Nama saya Khoirul Umam. Saya dari Sekolah Tinggi Ilmu Komputer (STIKOM) PGRI Banyuwangi.</p>";
+	echo "<p>Saat ini saya mengajar matakuliah Web Programming Fundamental</p>";
 	echo "Waktu di server menunjukkan hari ".date("D, d M Y H:i:s");
 ?>
