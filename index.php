7c082cd19b2da50b1826a4fb8073b6fc35350974
Khoirul Umam <khoirul.umam35@gmail.com>
2016-08-08 20:08:58 +0700

    Introducing
    Perkenalan diri

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	echo "<h1>Halo!</h1>";
+	echo "<p>Selamat datang di website saya. Nama saya Khoirul Umam. Saya dari STIKOM PGRI Banyuwangi</p>";
+	echo "Waktu di server menunjukkan hari ".date("D, d M Y H:i:s");
+?>
